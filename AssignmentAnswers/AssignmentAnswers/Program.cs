0d692f8f9177a4fe4eb68a57ad71e80524cace68
Tee787 <tawhiri.tamihana@gmail.comgit config --global user.name Tawhirimatea>
2018-11-12 11:42:53 +1300

    first commit

---
+++ AssignmentAnswers/AssignmentAnswers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentAnswers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            public static void Q1;
+        }
+        {
+            string answer = "x^2";
+
+            Console.WriteLine("Solve this (X+M)(X+N");
+            Console.Write("Please enter m: ");
+            int m = int.Parse(Console.ReadLine());
+            Console.Write("Please enter n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int b = m + n;
+            int c = m + n;
+
+            if (b > 0)
+                answer += $"+(b)x";
+
+
+        }
+    }
+}
